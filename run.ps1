e7ec9755b1d2ab4b369da66300eae62b1a5208b8
Matthew Jorgensen <matthew.jorgensen@clindex.com>
2026-06-05 16:07:32 -0500

    Update run script to read config

--- run.ps1
+++ run.ps1
@@ -1,6 +1,8 @@
 pwsh -Command {
     $env:OTEL_SDK_DISABLED = $true;
-    go run -v . -listen="127.0.0.1:2525" -hostname=localhost `
+    go run -v . `
+        -config=".\smtprelay.ini" `
+        -listen="127.0.0.1:2525" -hostname=localhost `
         -remote_host="email-smtp.us-east-1.amazonaws.com:587" `
         -remote_user="$(op read "op://Employee/AWS SES Dev Creds/username")" `
         -remote_pass="$(op read "op://Employee/AWS SES Dev Creds/password")"
